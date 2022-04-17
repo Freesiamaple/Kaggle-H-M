22fdd2ae1223e90d8730cec1494c874136e9f5f5
teruto725 <akiteruto@gmail.com>
2022-04-17 23:26:12 +0900

    0417

--- scripts/convert_pickle.py
+++ scripts/convert_pickle.py
@@ -10,7 +10,7 @@ def convert_csv_to_pkl(filename):
     df = pd.read_csv(f'{DIR}{filename}.csv')
     df.to_pickle(f'{DIR}{filename}.pkl')
 
-#convert_csv_to_pkl("articles")
-#convert_csv_to_pkl("customers")
-#convert_csv_to_pkl("sample_submission")
-#convert_csv_to_pkl("transactions_train")
+convert_csv_to_pkl("articles")
+convert_csv_to_pkl("customers")
+convert_csv_to_pkl("sample_submission")
+convert_csv_to_pkl("transactions_train")
